ddcaed8971214e5ab0cdaac1cda1f01581ee79b2
Kundana Addala <kundanaaddala@gmail.com>
2023-07-31 20:52:53 -0500

    Initial Commit

---
+++ esteeLauder/esteeLauderApp.swift
@@ -0,0 +1,17 @@
+//
+//  esteeLauderApp.swift
+//  esteeLauder
+//
+//  Created by Kundana Addala on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct esteeLauderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
